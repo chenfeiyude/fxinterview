72a8cccd414a886f328d58522e2a173317747505
Feiyu <chenfeiyu0402@gmail.com>
2017-06-11 01:28:42 +0100

    updated readme setup steps and updated setting

--- README.md
+++ README.md
@@ -11,12 +11,31 @@ This README would normally document whatever steps are necessary to get your app
 ### How do I get set up? ###
 
 * Summary of set up
-
+MAC-OS
 1. install python3.6 official release.
-2. install django by pip:
+2. install django by pip for coding:
 pip3.6 install Django
 3. create a project with django:
 python3.6 -m django startproject fxinterview
+4. create virtual environment:
+python3.6 -m venv /path/to/fxinterview/venv
+5. start venv:
+source venv/bin/activate
+6. install django in evnv:
+pip3.6 install django
+7. install mysqlclient
+(brew unlink mysql)
+brew install mysql-connector-c
+(brew unlink mysql-connector-c)
+(brew link mysql)
+pip3.6 install mysqlclient
+8. migrate db
+(create database fxinterview in your mysql)
+python3.6 manage.py migrate
+6. start server:
+python3.6 manage.py runserver
+
+
 
 * Configuration
 * Dependencies
--- fxinterview/settings.py
+++ fxinterview/settings.py
@@ -73,13 +73,27 @@ WSGI_APPLICATION = 'fxinterview.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mydatabase',
+#         'USER': 'mydatabaseuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fxinterview',
+        'USER': 'mydatabase',
+        'PASSWORD': 'mypassword',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -105,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Dublin'
 
 USE_I18N = True
 
