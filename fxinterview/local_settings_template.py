467c99e3fab811087f8dc1e7cb063150927cf08b
Feiyu <chenfeiyu0402@gmail.com>
2017-06-27 19:15:12 +0100

    updated settings

---
+++ fxinterview/local_settings_template.py
@@ -0,0 +1,14 @@
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
--- fxinterview/settings.py
+++ fxinterview/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+from .local_settings import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'l3#nmrq8d%(!gzpr9-u*!#=0pnr))3)!pvo3tc2rdw6xt)_0xu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.fxinterview.com']
+ALLOWED_HOSTS = ['www.fxinterview.com', 'localhost']
 
 
 # Application definition
@@ -69,21 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'fxinterview.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
