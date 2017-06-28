7f34a57c8623261ab47604e5394de91f510136d0
Feiyu <chenfeiyu0402@gmail.com>
2017-06-29 00:35:01 +0100

    added basic url and views for interviewee

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^interviewee/application/(?P<application_question_id>[0-9]+)$', views.view_application_questions, name='view_application_questions'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import ApplicationQuestion
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def view_application_questions(request, application_question_id):
+    application_question = get_object_or_404(ApplicationQuestion, pk=application_question_id)
+
+    return render(request, 'interviewee/view_application_questions.html', {'application_question': application_question})
