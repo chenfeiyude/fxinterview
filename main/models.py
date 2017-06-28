b7a141bdd80df62fa807c812409e0a28085798ea
Feiyu <chenfeiyu0402@gmail.com>
2017-06-29 00:19:21 +0100

    added intial models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,60 @@
 from django.db import models
-
 # Create your models here.
+
+
+class ContactDetails(models.Model):
+    address1 = models.CharField(max_length=100)
+    address2 = models.CharField(max_length=100)
+    address3 = models.CharField(max_length=100)
+    address4 = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField
+    contact = models.ForeignKey(ContactDetails)
+    updated = models.DateTimeField('company last update time')
+
+
+class CompanyStaff(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    # staff = models.ForeignKey(User, on_delete=models.CASCADE) # TODO enable this once user model is created
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    updated = models.DateTimeField('question last update time')
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField
+    updated = models.DateTimeField('question last update time')
+
+
+class JobQuestion(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+class ApplicationQuestion(models.Model):
+    # interviewee = models.ForeignKey(User, on_delete=models.CASCADE) # TODO enable this once user model is created
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    deadline = models.DateTimeField('Application deadline')
+    start_time = models.DateTimeField('Start answer time')
+    end_time = models.DateTimeField('End answer time')
+
+
+class Answer(models.Model):
+    application_question = models.ForeignKey(ApplicationQuestion, on_delete=models.CASCADE)
+    job_question = models.ForeignKey(JobQuestion, on_delete=models.CASCADE)
+    answer = models.TextField
+
+
+
+
+
