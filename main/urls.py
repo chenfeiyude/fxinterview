ba9e3ab6e0426b691d93cecf0d397674588760af
Feiyu <chenfeiyu0402@gmail.com>
2017-06-29 00:42:31 +0100

    added comment

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+# name space for url
+# e.g you can call {% url 'main.view_application_questions' xxx.id %} instead of hard code url in html
+
+app_name = 'main'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^interviewee/application/(?P<application_question_id>[0-9]+)$', views.view_application_questions, name='view_application_questions'),
