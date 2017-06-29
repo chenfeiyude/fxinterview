3f0ed961ac15a4a6f3dafdbdd200f9c3a6176650
xiangxu <xuxiang1990619@gmail.com>
2017-06-29 23:36:27 +0100

    updated readme setup steps and updated setting

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 # name space for url
 # e.g you can call {% url 'main.view_application_questions' xxx.id %} instead of hard code url in html
@@ -9,5 +10,6 @@ app_name = 'main'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.login, {'template_name': 'main/login.html'}),
     url(r'^interviewee/application/(?P<application_question_id>[0-9]+)$', views.view_application_questions, name='view_application_questions'),
 ]
