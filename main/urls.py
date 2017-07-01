553f5b715f20f16352897ecf3da28be7766a8739
xiangxu <xuxiang1990619@gmail.com>
2017-07-01 11:36:38 +0100

    update login

--- main/urls.py
+++ main/urls.py
@@ -7,10 +7,10 @@ from django.contrib.auth import views as auth_views
 # e.g you can call {% url 'main.view_application_questions' xxx.id %} instead of hard code url in html
 
 app_name = 'main'
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # http://localhost:8000/login/
     url(r'^login/$', auth_views.login, {'template_name': 'main/login.html'}),
-    url(r'^interviewee/application/(?P<application_question_id>[0-9]+)$', views.view_application_questions, name='view_application_questions'),
+    # http://localhost:8000/application/1?interviewee_email=xuxiang1990619@gmail.com
     url(r'^application/(?P<application_question_id>[0-9]+)$', views.view_application_questions, name='view_application_questions'),
 ]
