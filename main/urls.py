7376974be3f12b08b369bf52a9dc4ba9516f7051
Feiyu <chenfeiyu0402@gmail.com>
2017-07-03 00:48:15 +0100

    updated comment

--- main/urls.py
+++ main/urls.py
@@ -8,12 +8,13 @@ from django.contrib.auth import views as auth_views
 
 app_name = 'main'
 urlpatterns = [
+    # http://localhost:8000/
     url(r'^$', views.index, name='index'),
     # http://localhost:8000/login/
     url(r'^login/$', auth_views.login, {'template_name': 'main/login.html'}, name='login'),
     # http://localhost:8000/logout/
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    # http://localhost:8000/profile/
+    # http://localhost:8000/accounts/home
     url(r'^accounts/home', views.check_user_role, name='index'),
     # http://localhost:8000/application/1?interviewee_email=xuxiang1990619@gmail.com
     url(r'^application/(?P<application_question_id>[0-9]+)$', views.view_application_questions, name='view_application_questions'),
