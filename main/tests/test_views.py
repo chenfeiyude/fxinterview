6962661954ba7c942161c7d6f433879b96946ecd
Feiyu <chenfeiyu0402@gmail.com>
2017-07-06 00:54:43 +0100

    added test not found

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+
+
+class ApplicationViewTestCase(TestCase):
+
+    def test_not_found(self):
+        """Test if no email sent and the view return 404 not found error"""
+        url = reverse("main:view_application_questions",
+                      kwargs={'application_question_id':1})
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 404)
