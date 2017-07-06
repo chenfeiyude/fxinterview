599089171b5b874ead6129e5580dc057fb3cc5f2
Feiyu <chenfeiyu0402@gmail.com>
2017-07-06 01:09:55 +0100

    improved tests

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -9,13 +9,17 @@ class ApplicationQuestionTestCase(TestCase):
 
     def setUp(self):
         ContactDetails.objects.create(address1='address1', address2='address2')
-        Company.objects.create(name='test company', contact=ContactDetails.objects.get(pk=1))
-        Job.objects.create(name='test job', company=Company.objects.get(pk=1))
-        ApplicationQuestion.objects.create(interviewee_email='test@fxinterview.com', job=Job.objects.get(pk=1))
+        Company.objects.create(name='test company',
+                               contact=ContactDetails.objects.get(address1='address1', address2='address2'))
+        Job.objects.create(name='test job', company=Company.objects.get(name='test company'))
+        ApplicationQuestion.objects.create(interviewee_email='test@fxinterview.com',
+                                           job=Job.objects.get(name='test job'))
         Question.objects.create(name='test question1')
         Question.objects.create(name='test question2')
-        JobQuestion.objects.create(job=Job.objects.get(pk=1), question=Question.objects.get(pk=1))
-        JobQuestion.objects.create(job=Job.objects.get(pk=1), question=Question.objects.get(pk=2))
+        JobQuestion.objects.create(job=Job.objects.get(name='test job'),
+                                   question=Question.objects.get(name='test question1'))
+        JobQuestion.objects.create(job=Job.objects.get(name='test job'),
+                                   question=Question.objects.get(name='test question2'))
 
     def test_model_relationship(self):
         """Test application question and other models's relationship"""
@@ -26,6 +30,9 @@ class ApplicationQuestionTestCase(TestCase):
         self.assertEqual(application_question.job.company.name, 'test company')
         self.assertEqual(application_question.job.company.contact.address1, 'address1')
 
+    def test_getting_questions(self):
+        """Test loading questions by application object"""
+        application_question = ApplicationQuestion.objects.get(interviewee_email="test@fxinterview.com")
         job_questions = JobQuestion.objects.filter(job=application_question.job)
 
         self.assertEqual(job_questions[0].question.name, 'test question1')
--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,15 +1,36 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from ..models import *
+
 # Create your tests here.
 
 
 class ApplicationViewTestCase(TestCase):
 
+    def setUp(self):
+        ContactDetails.objects.create(address1='address1', address2='address2')
+        Company.objects.create(name='test company', contact=ContactDetails.objects.get(address1='address1', address2='address2'))
+        Job.objects.create(name='test job', company=Company.objects.get(name='test company'))
+        ApplicationQuestion.objects.create(interviewee_email='test@fxinterview.com', job=Job.objects.get(name='test job'))
+        Question.objects.create(name='test question1')
+        Question.objects.create(name='test question2')
+        JobQuestion.objects.create(job=Job.objects.get(name='test job'), question=Question.objects.get(name='test question1'))
+        JobQuestion.objects.create(job=Job.objects.get(name='test job'), question=Question.objects.get(name='test question2'))
+
     def test_not_found(self):
         """Test if no email sent and the view return 404 not found error"""
-        url = reverse("main:view_application_questions",
-                      kwargs={'application_question_id':1})
+        application_question = ApplicationQuestion.objects.get(interviewee_email='test@fxinterview.com')
+        url = reverse("main:view_application_questions", kwargs={'application_question_id':application_question.id})
         resp = self.client.get(url)
 
         self.assertEqual(resp.status_code, 404)
+
+    def test_found(self):
+        """Test if found the application question view by sending email and application id"""
+        application_question = ApplicationQuestion.objects.get(interviewee_email='test@fxinterview.com')
+        url = "%s?interviewee_email=test@fxinterview.com" \
+              % reverse("main:view_application_questions", kwargs={'application_question_id': application_question.id} )
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
