aca1375e277df21e5c190c704eb9840fdb7c2f7f
Feiyu <chenfeiyu0402@gmail.com>
2017-07-15 15:52:43 +0100

    added start answer function

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,8 @@ urlpatterns = [
     url(r'^accounts/home', views.check_user_role, name='view_home'),
     # http://localhost:8000/application/2?interviewee_email=interviewee@fxinterview.com
     url(r'^application/(?P<application_question_id>[0-9]+)$', views.view_application_questions, name='view_application_questions'),
+    # http://localhost:8000/application/start_answer$
+    url(r'^application/start_answer', views.start_answer, name='start_answer'),
     # http://localhost:8000/application/submit_answer
     url(r'^application/submit_answer', views.submit_answer, name='submit_answer'),
 
