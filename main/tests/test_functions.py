c6b78e21885bcddc55bc9f984871d682ea407a20
Feiyu <chenfeiyu0402@gmail.com>
2017-08-22 18:53:38 +0100

    added java compiler test

--- main/tests/test_functions.py
+++ main/tests/test_functions.py
@@ -1,7 +1,6 @@
 import os, sys, logging
 from django.test import TestCase
-from ..compiler import fx_python_compiler
-from ..utils import fx_file_utils
+from ..compiler import fx_python_compiler, fx_java_compiler
 
 
 class FXPythonCompilerTestCase(TestCase):
@@ -13,3 +12,13 @@ class FXPythonCompilerTestCase(TestCase):
         self.assertIsNotNone(results)
         self.assertEqual(results['code'], "Success")
         self.assertEqual(results['output'], "2\n")
+
+class FXJavaCompilerTestCase(TestCase):
+
+    def test_run_code(self):
+        """Test if run code methods return the expected results"""
+        code = 'public class hello {public static void main(String []args) {System.out.println("Hello World");}}'
+        results = fx_java_compiler.run_code(code)
+        self.assertIsNotNone(results)
+        self.assertEqual(results['code'], "Success")
+        self.assertEqual(results['output'], "Hello World\n")
