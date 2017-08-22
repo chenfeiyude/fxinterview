1f50c9d80bbbd7a3cf0556bb6e963e438f054df3
Feiyu <chenfeiyu0402@gmail.com>
2017-08-22 18:31:07 +0100

    fxied java compiler

--- main/compiler/fx_java_compiler.py
+++ main/compiler/fx_java_compiler.py
@@ -2,19 +2,22 @@ import os, sys, subprocess, time
 import logging
 from ..utils import fx_file_utils, fx_string_utils, fx_constants
 
-# file name
-file_num = int(time.time() * 1000)
+
 bash_file = '%s/main/compiler/test.sh'% os.getcwd()
 
 # java file name
-def get_file_name():
-    global file_num
-    return 'test_%d.java' % file_num
+def get_file_name(code):
+    if code:
+        if 'public class' in code and '{' in code:
+            file_name = code.split('public class ')[1].split('{')[0].strip()
+        else:
+            file_name = "test"
+        return '%s.java' % file_name
 
 
 def run_code(code):
     result = dict()
-    file_name = get_file_name()
+    file_name = get_file_name(code)
     file_dir = fx_file_utils.make_temp_dir()
     file_path = fx_file_utils.write_file(file_dir, file_name, code)
     try:
@@ -27,8 +30,8 @@ def run_code(code):
         result["output"] = fx_string_utils.decode_utf_8(e.output)
         return result
     else:
-        # out_data = fx_string_utils.decode_utf_8(subprocess.check_output([bash_file, file_dir, fx_constants.JAVA_EXEC, file_name],stderr=subprocess.STDOUT, timeout=5))
-        out_data = fx_string_utils.decode_utf_8(subprocess.check_output([fx_constants.JAVA_EXEC, file_path], stderr=subprocess.STDOUT, timeout=5))
+        java_name = file_name.replace('.java', '')
+        out_data = fx_string_utils.decode_utf_8(subprocess.check_output([bash_file, file_dir, fx_constants.JAVA_EXEC, java_name],stderr=subprocess.STDOUT, timeout=5))
 
         # return success data
         result['output'] = out_data
