4c6fb3b6867b02903c38676519a8ec96aa74b454
Feiyu <chenfeiyu0402@gmail.com>
2017-09-30 22:34:41 +0100

    added comments

--- fxinterview/urls.py
+++ fxinterview/urls.py
@@ -18,17 +18,24 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # http://localhost:8000/admin/
     url(r'^admin/', admin.site.urls),
     url(r'^', include('main.urls')),
-    # http://localhost:8000/login/
+    # http://localhost:8000/accounts/login/
     url(r'^accounts/login/$', auth_views.login, {'template_name': 'main/login.html'}, name='login'),
-    # http://localhost:8000/logout/
+    # http://localhost:8000/accounts/logout/
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    # http://localhost:8000/password_change/
     url(r'^password_change/$', auth_views.PasswordChangeView.as_view(success_url='/password_change/done'), name='password_change'),
+    # http://localhost:8000/password_change/done
     url(r'^password_change/done/$', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # http://localhost:8000/password_reset/
     url(r'^password_reset/$', auth_views.PasswordResetView.as_view(success_url='/password_reset/done', from_email='support@fxinterview.com'), name='password_reset'),
+    # http://localhost:8000/password_reset/done
     url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # http://localhost:8000/password_reset/confirm
     url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(success_url='/password_reset/complete'), name='password_reset_confirm'),
+    # http://localhost:8000/password_reset/complete
     url(r'^password_reset/complete/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
